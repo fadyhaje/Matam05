8b9db903411b5ee1033234a3311d6abc09eff902
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2023-01-26 00:41:34 +0200

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,5 +1,6 @@
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
+import os
 class CaesarCipher:
     def __init__(self,k):
         self.k=k
@@ -33,7 +34,7 @@ class VigenereCipher:
             if isalpha(str[i]):
                 cesar.k=self.k[counter%len(self.k)]
                 new_str.append(cesar.encrypt(str[i]))
-                counter++;
+                counter+=1;
         return new_str
     def decrypt(self,str):
         temp_k=[-elemnt for element in self.k]
@@ -45,7 +46,7 @@ class VigenereCipher:
             if isalpha(str[i]):
                 cesar.k=stemp_k[counter%len(self.k)]
                 new_str.append(cesar.decrypt(str[i]))
-                counter++;
+                counter+=1;
         return new_str
         
 def getVigenereFromStr(str):
@@ -55,3 +56,36 @@ def getVigenereFromStr(str):
             number=ord(elem) # char to int
             temp_k.append(number)
     return VigenereCipher(temp_k)
+    
+    
+    
+def turn_ch_to_num(elem)
+    if 'a'<=elem<='z' 
+        return elem-'a'
+    elif  'A'<=elem<='Z'
+        return elem-'A'-('z'-'a')+1
+            
+def processDirectory(dir_path::str)   
+    config_path=os.path.join(dir_path,config.json)
+    with open (config_path,'r') as f:
+        loaded_config=json.load(f)
+    key=loaded_config['key']
+    if  loaded_config['type']=='Vigenere'
+        obj=VigenereCipher(key)
+    else  
+        obj=Caesar(key)
+    mode=loaded_config['mode']
+    
+    for file in os.listdir(dir_path)    
+        if not file==config.json
+            with open(file,'r')  as new_f
+            temp_str=f.read()
+            if mode=="encrypt" and (os.path.splitext(file)[1]==".txt")
+                new_f=os.path.join(os.path.split(file)[0],(os.path.split(file)[1]+".enc")
+                with open(new_f,'w')  as f 
+                f.write(obj.encrypt(temp_str))
+            elif mode=="decrypt" and (os.path.splitext(file)[1]==".enc") 
+                new_f=os.path.split(file)[1]+".txt"
+                with open(new_f,'w')  as f 
+                f.write(obj.decrypt(temp_str))
+    return None                                   
